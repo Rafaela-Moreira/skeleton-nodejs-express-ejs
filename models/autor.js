60348a29b7377d25b2af50cdb2008ab927a4298c
Rafaela Moreira <86133113+Rafaela-Moreira@users.noreply.github.com>
2022-03-16 19:42:11 +0000

    Adicionado o item "inserir"

--- models/autor.js
+++ models/autor.js
@@ -3,17 +3,14 @@ const db = require("../db")
 class Autor {
   static async selecionar(){
     const connect = await db.connect();
-    return await connect.query("SELECT * FROM autores");
+    return await connect.query("select * from autores");
   }
-
-  static async inserir(data){
+ static async inserir(data){
     const connect = await db.connect();
-    const sql = "INSERT INTO autores(nome, sobrenome, data_nascimento) VALUES($1, $2, $3)";
+    const sql = "insert into autores (nome, sobrenome, data_nascimento) values ($1, $2, $3)";
     const values = [data.nome, data.sobrenome, data.data_nascimento]
     return await connect.query(sql, values);
   }
-
-
 }
 
 module.exports = Autor;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
-const Autor = require("../models/autor");
+const Autor = require ("../models/autor");
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const autores = await Autor.selecionar()
-  res.json(autores.rows);
-  res.render('index', { title: 'Express' });
+  res.json (autores.rows);
+  /res.render('index', { title: 'Express' });/
 });
+
+router.get('/inserir', async function(req, res, next) {
+  let autor = {
+    nome: "Margarida",
+    sobrenome: "Silva",
+    data_nascimento: "1999-07-01"
+  };
+  const autores = await Autor.inserir(autor);
+  res.json(autores.rows);
+});
+
 
 module.exports = router;
