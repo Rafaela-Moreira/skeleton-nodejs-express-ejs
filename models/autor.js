a814d69bec6e1f6cf1003c5104176c81112b4680
Rafaela Moreira <86133113+Rafaela-Moreira@users.noreply.github.com>
2022-03-16 21:02:47 +0000

    Adicionado o item "atualizar"

--- models/autor.js
+++ models/autor.js
@@ -11,6 +11,21 @@ class Autor {
     const values = [data.nome, data.sobrenome, data.data_nascimento]
     return await connect.query(sql, values);
   }
+
+ static async atualizar(id){
+    const connect = await db.connect();
+    const sql = "update autores SET nome=$1 where id = $1)";
+    const values = [id]
+    return await connect.query(sql, values);
+  }
+  
+ static async deletar(id){
+    const connect = await db.connect();
+    const sql = "delete from autores where id = $1)";
+    const values = [id]
+    return await connect.query(sql, values);
+  }
+
 }
 
 module.exports = Autor;
--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,20 @@ router.get('/', async function(req, res, next) {
 
 router.get('/inserir', async function(req, res, next) {
   let autor = {
-    nome: "Margarida",
+    nome: "Luiza",
     sobrenome: "Silva",
-    data_nascimento: "1999-07-01"
+    data_nascimento: "1991-09-01"
   };
   const autores = await Autor.inserir(autor);
   res.json(autores.rows);
 });
 
+router.get('/atualizar', async function(req, res, next) {
+  let autor = {
+    id: "3"
+  }
+  const autores = await Autor.inserir(autor);
+  res.json(autores.rows);
+});
 
 module.exports = router;
